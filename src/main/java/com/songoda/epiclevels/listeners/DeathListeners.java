c9d16988c50a4dd3ffef999d981947b1634ce1bf
Brianna <bri@songoda.com>
2019-12-16 18:35:49 -0500

    Missing placeholder.

--- src/main/java/com/songoda/epiclevels/listeners/DeathListeners.java
+++ src/main/java/com/songoda/epiclevels/listeners/DeathListeners.java
@@ -58,20 +58,20 @@ public class DeathListeners implements Listener {
         double expPlayer = Settings.EXP_PLAYER.getDouble();
         double expMob = Settings.EXP_MOB.getDouble();
 
-        Player player = event.getEntity().getKiller();
-        EPlayer ePlayer = plugin.getPlayerManager().getPlayer(player);
+        Player killer = event.getEntity().getKiller();
+        EPlayer eKiller = plugin.getPlayerManager().getPlayer(killer);
 
         if (event.getEntity() instanceof Player) {
             Player killed = (Player) event.getEntity();
             EPlayer eKilled = plugin.getPlayerManager().getPlayer(killed);
 
-            if (player == killed) return;
+            if (killer == killed) return;
 
-            if (!ePlayer.canGainExperience(killed.getUniqueId()) && Settings.ANTI_GRINDER.getBoolean()) {
+            if (!eKiller.canGainExperience(killed.getUniqueId()) && Settings.ANTI_GRINDER.getBoolean()) {
                 if (Settings.GRINDER_ALERT.getBoolean())
                     plugin.getLocale().getMessage("event.antigrinder.trigger")
                             .processPlaceholder("killed", killed.getPlayer().getName())
-                            .sendPrefixedMessage(player);
+                            .sendPrefixedMessage(killer);
                 return;
             }
 
@@ -81,14 +81,15 @@ public class DeathListeners implements Listener {
             double killedExpAfter = eKilled.addExperience(0L - Settings.EXP_DEATH.getDouble());
 
             if (Settings.SEND_DEATH_MESSAGE.getBoolean())
-                plugin.getLocale().getMessage("event.player.death").processPlaceholder("name", ChatColor.stripColor(player.getName()))
+                plugin.getLocale().getMessage("event.player.death").processPlaceholder("name", ChatColor.stripColor(killer.getName()))
                         .processPlaceholder("exp", Methods.formatDecimal(-(killedExpAfter - killedExpBefore))).sendPrefixedMessage(killed);
 
             if (Settings.SEND_BROADCAST_DEATH_MESSAGE.getBoolean())
-                for (Player pl : Bukkit.getOnlinePlayers().stream().filter(p -> p != player && p != killed).collect(Collectors.toList()))
+                for (Player pl : Bukkit.getOnlinePlayers().stream().filter(p -> p != killer && p != killed).collect(Collectors.toList()))
                     plugin.getLocale().getMessage("event.player.death.broadcast")
+                            .processPlaceholder("killer", killer.getName())
                             .processPlaceholder("killed", killed.getName())
-                            .processPlaceholder("exp", player.getName())
+                            .processPlaceholder("exp", killer.getName())
                             .sendPrefixedMessage(pl);
 
             if (Settings.SEND_KILLSTREAK_BROKEN_MESSAGE.getBoolean() && eKilled.getKillstreak() >= Settings.SEND_KILLSTREAK_ALERTS_AFTER.getInt()) {
@@ -98,55 +99,55 @@ public class DeathListeners implements Listener {
                 plugin.getLocale().getMessage("event.killstreak.broke")
                         .processPlaceholder("killed", killed.getName())
                         .processPlaceholder("streak", eKilled.getKillstreak())
-                        .sendPrefixedMessage(player);
+                        .sendPrefixedMessage(killer);
             }
 
             if (Settings.SEND_BROADCAST_BROKEN_KILLSTREAK.getBoolean() && eKilled.getKillstreak() >= Settings.SEND_KILLSTREAK_ALERTS_AFTER.getInt())
-                for (Player pl : Bukkit.getOnlinePlayers().stream().filter(p -> p != player && p != killed).collect(Collectors.toList()))
+                for (Player pl : Bukkit.getOnlinePlayers().stream().filter(p -> p != killer && p != killed).collect(Collectors.toList()))
                     plugin.getLocale().getMessage("event.killstreak.brokenannounce")
-                            .processPlaceholder("player", player.getName())
+                            .processPlaceholder("player", killer.getName())
                             .processPlaceholder("killed", killed.getName())
                             .processPlaceholder("streak", eKilled.getKillstreak())
                             .sendPrefixedMessage(pl);
 
             eKilled.resetKillstreak();
 
-            ePlayer.increaseKillstreak();
-            ePlayer.addPlayerKill(killed.getUniqueId());
+            eKiller.increaseKillstreak();
+            eKiller.addPlayerKill(killed.getUniqueId());
 
-            plugin.getDataManager().updatePlayer(ePlayer);
+            plugin.getDataManager().updatePlayer(eKiller);
             plugin.getDataManager().updatePlayer(eKilled);
 
             int every = Settings.RUN_KILLSTREAK_EVERY.getInt();
-            if (every != 0 && ePlayer.getKillstreak() % every == 0) {
+            if (every != 0 && eKiller.getKillstreak() % every == 0) {
                 Killstreak def = plugin.getKillstreakManager().getKillstreak(-1);
                 if (def != null)
-                    Rewards.run(def.getRewards(), player, ePlayer.getKillstreak(), true);
-                if (plugin.getKillstreakManager().getKillstreak(ePlayer.getKillstreak()) == null) return;
-                Rewards.run(plugin.getKillstreakManager().getKillstreak(ePlayer.getKillstreak()).getRewards(), player, ePlayer.getKillstreak(), true);
+                    Rewards.run(def.getRewards(), killer, eKiller.getKillstreak(), true);
+                if (plugin.getKillstreakManager().getKillstreak(eKiller.getKillstreak()) == null) return;
+                Rewards.run(plugin.getKillstreakManager().getKillstreak(eKiller.getKillstreak()).getRewards(), killer, eKiller.getKillstreak(), true);
             }
 
-            double playerExpBefore = ePlayer.getExperience();
-            double playerExpAfter = ePlayer.addExperience(expPlayer);
+            double playerExpBefore = eKiller.getExperience();
+            double playerExpAfter = eKiller.addExperience(expPlayer);
 
             if (Settings.SEND_PLAYER_KILL_MESSAGE.getBoolean())
                 plugin.getLocale().getMessage("event.player.killed")
                         .processPlaceholder("name", ChatColor.stripColor(killed.getDisplayName()))
                         .processPlaceholder("exp", Methods.formatDecimal(playerExpAfter - playerExpBefore))
-                        .sendPrefixedMessage(player);
+                        .sendPrefixedMessage(killer);
 
         } else {
-            ePlayer.addMobKill();
-            double playerExpBefore = ePlayer.getExperience();
-            double playerExpAfter = ePlayer.addExperience(expMob);
+            eKiller.addMobKill();
+            double playerExpBefore = eKiller.getExperience();
+            double playerExpAfter = eKiller.addExperience(expMob);
 
-            plugin.getDataManager().updatePlayer(ePlayer);
+            plugin.getDataManager().updatePlayer(eKiller);
 
             if (Settings.SEND_MOB_KILL_MESSAGE.getBoolean()) {
                 plugin.getLocale().getMessage("event.mob.killed")
                         .processPlaceholder("type", Methods.formatText(event.getEntity().getType().name(), true))
                         .processPlaceholder("exp", Methods.formatDecimal(playerExpAfter - playerExpBefore))
-                        .sendPrefixedMessage(player);
+                        .sendPrefixedMessage(killer);
             }
         }
 
