1a2761160dbbd65a940b87d0d95f2f489e5b5986
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2022-01-06 18:27:17 +0100

    Safety of collision

--- src/main/java/com/songoda/epiclevels/killstreaks/Killstreak.java
+++ src/main/java/com/songoda/epiclevels/killstreaks/Killstreak.java
@@ -2,6 +2,7 @@ package com.songoda.epiclevels.killstreaks;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Killstreak {
     private final int killstreak;
@@ -18,5 +19,18 @@ public class Killstreak {
 
     public List<String> getRewards() {
         return Collections.unmodifiableList(rewards);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Killstreak that = (Killstreak) o;
+        return killstreak == that.killstreak && rewards.equals(that.rewards);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(killstreak, rewards);
     }
 }
